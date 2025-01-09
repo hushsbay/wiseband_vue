7c54c45437833c1e8c42810e9e9c36ee7a238456
hushsbay <hushsbay@gmail.com>
2025-01-09 20:25:46 +0900

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main>
     <h1 class="text-3xl font-bold underline">
-        Hello world!!!
+        Hello world!!!@@@
       </h1>
     <TheWelcome />
   </main>
