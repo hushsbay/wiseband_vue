57cd89903100f69adc6396669514fbb662f494cf
hushsbay <hushsbay@gmail.com>
2025-01-09 20:16:59 +0900

    .

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!!!
+      </h1>
+    <TheWelcome />
+  </main>
+</template>
