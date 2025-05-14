72acad4547e1cfd05de37e80bbd63e444b24b37c
hushsbay <hushsbay@gmail.com>
2025-05-14 21:13:11 +0900

    .

--- src/components/OrgTree.vue
+++ src/components/OrgTree.vue
@@ -57,8 +57,8 @@
                 }
             }
         }
-        debugger
-        debugger
+        //debugger
+        //debugger
     }
 
     function procNode(node, rowNext, kind) {
@@ -107,6 +107,44 @@
     function rowLeave(row) { //css만으로 처리가 힘들어 코딩으로 구현
         row.hover = false
     }
+
+    function clickNode(row, idx) {
+        const hasChild = row.haschild
+        const nodekind = row.nodekind
+        const expanded = row.expanded
+        const lvl = parseInt(row.LVL)
+        const rowNext = orglist.value[idx + 1]
+        if (nodekind == "U" || !hasChild) return
+        if (rowNext.LVL <= lvl) return
+        const len = orglist.value.length
+        let j = 0
+        for (let i = idx + 1; i < len; i++) {
+            const rowCur = orglist.value[i]
+            const lvlNext = parseInt(rowCur.LVL)
+            if (lvlNext <= lvl) break
+            if (expanded) {
+                rowCur.dispstatePrev = rowCur.dispstate
+                rowCur.dispstate = "none"
+            } else { //펼쳐야 함
+                if (lvlNext == lvl + 1) {
+                    rowCur.dispstate = "flex"
+                } else {
+                    rowCur.dispstate = rowCur.dispstatePrev
+                }
+            }
+            j += 1
+        }  
+        if (j > 0) procExpCol(expanded, row, idx)
+    }
+
+    function procExpCol(expanded, row) {
+        if (row.dispstate == "none") row.dispstate = "flex"
+        if (expanded) { //펼쳐져 있으면 접기 표시
+            row.expanded = false
+        } else {
+            row.expanded = true
+        }
+    }
 </script>
 
 <template>    
@@ -125,11 +163,11 @@
         <div class="chan_center">
             <div class="chan_center_header">
                 <div class="chan_center_header_left">
-                    <input type="search" v-model="frYm" @keyup.enter="procSearchMedia(true)" style="width:90px;margin-right:2px" placeholder="YYYYMM" /> - 
+                    <!-- <input type="search" v-model="frYm" @keyup.enter="procSearchMedia(true)" style="width:90px;margin-right:2px" placeholder="YYYYMM" /> - 
                     <input type="search" v-model="toYm" @keyup.enter="procSearchMedia(true)" style="width:90px;margin-left:2px" placeholder="YYYYMM" />
                     <input type="search" v-model="authorNm" @keyup.enter="procSearchMedia(true)" style="width:100px" placeholder="전송자" />
                     <input type="search" v-model="searchText" @keyup.enter="procSearchMedia(true)" style="width:120px" placeholder="본문" />
-                    <input type="search" v-model="fileExt" @keyup.enter="procSearchMedia(true)" style="width:60px" placeholder="확장자" />
+                    <input type="search" v-model="fileExt" @keyup.enter="procSearchMedia(true)" style="width:60px" placeholder="확장자" /> -->
                     <div class="coImgBtn" @click="procSearchMedia(true)">
                         <img :src="gst.html.getImageUrl('search.png')" class="btn_img">
                         <span class="btn_spn">검색</span>
@@ -142,18 +180,16 @@
                 <div class="chan_center_header_right"></div>
             </div>
             <div class="chan_center_body" ref="scrollArea">
-                <div v-for="(row, idx) in orglist" class="org_body" @mouseenter="rowEnter(row)" @mouseleave="rowLeave(row)">
+                <div v-for="(row, idx) in orglist" class="org_body" 
+                    @mouseenter="rowEnter(row)" @mouseleave="rowLeave(row)" @click="clickNode(row, idx)">
                     <div v-if="!row.USER_ID" :style="{ paddingLeft: row.paddingleft, display: row.dispstate }"
-                        style="width:100%;height:40px;align-items:center">
+                        style="width:100%;height:40px;align-items:center;border-bottom:1px solid lightgray;">
                         <div>{{ row.ORG_NM }}</div>
-                        <div>{{ row.SEQ }}</div>
-                        <div>===={{ row.dispstate }}</div>
                         <div>===={{ row.expanded }}</div>
                     </div>
                     <div v-else :style="{ paddingLeft: row.paddingleft, display: row.dispstate }"
-                        style="width:100%;height:40px;align-items:center">
+                        style="width:100%;height:40px;align-items:center;border-bottom:1px solid lightgray;">
                         <div>{{ row.USER_NM }}</div>
-                        <div>===={{ row.dispstate }}</div>
                         <div>===={{ row.expanded }}</div>
                     </div>
                 </div>
@@ -189,7 +225,7 @@
         width:100%;height:100%;margin-bottom:5px;display:flex;flex-direction:column;flex:1;overflow-y:auto;background:beige
     }
     .org_body {
-        width:100%;display:flex;flex-direction:column;border:1px solid lightgray;cursor:pointer
+        width:100%;display:flex;cursor:pointer
     }
     .item {  				
         padding:5px;display:flex;flex-direction:column;justify-content:center;align-items:center;border:1px solid lightgray
