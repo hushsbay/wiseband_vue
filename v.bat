a1c11f68dac376c488886ada642916b7f427272f
hushsbay <hushsbay@gmail.com>
2025-09-18 13:39:04 +0900

    .

--- v.bat
+++ v.bat
@@ -1,4 +1,4 @@
 del \src\git\wiseband_nest\public\assets\index*.js
 del \src\git\wiseband_nest\public\assets\index*.css
-copy /y d:\src\git\wiseband_vue\dist\assets\* d:\src\git\wiseband_nest\public\assets\
-copy /y d:\src\git\wiseband_vue\dist\index.html d:\src\git\wiseband_nest\public\index.html
+copy /y c:\src\git\wiseband_vue\dist\assets\* c:\src\git\wiseband_nest\public\assets\
+copy /y c:\src\git\wiseband_vue\dist\index.html c:\src\git\wiseband_nest\public\index.html
